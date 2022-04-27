ac5afd2bc6f397f218d2ff5f581f0b37a99ee730
Esteban Canteros <estebanacanteros@gmail.com>
2022-04-27 08:48:57 -0300

    task

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,31 @@
 import ActiveLink from './ActiveLink'
 import styles from './Navbar.module.css'
+
+const menuItems = [
+  {
+    text: 'Home',
+    href: '/',
+  },
+  {
+    text: 'About',
+    href: '/about',
+  },
+  {
+    text: 'Contact',
+    href: '/contact',
+  },
+  {
+    text: 'Pricing',
+    href: '/pricing',
+  },
+]
+
 const Navbar = () => {
   return (
     <nav className={styles['menu-container']}>
-      <ActiveLink text="Home" href='/' />
-      <ActiveLink text="About" href='/about' />
-      <ActiveLink text="Contact" href='/contact' />
+      {menuItems.map(({ href, text }) => (
+        <ActiveLink href={href} text={text} key={href} />
+      ))}
     </nav>
   )
 }
